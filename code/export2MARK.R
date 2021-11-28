a1728c1a1d7a3547b6c947552ec2880c53aa99b3
Daniel Thonon <d.thonon9@gmail.com>
2021-11-28 23:51:35 +0100

    Changing dead (2) to recapture (1)

--- code/export2MARK.R
+++ code/export2MARK.R
@@ -3,6 +3,12 @@
 # Données capture-recapture hiver 1997/1998 -> hiver 2018/2019
 dat <- readRDS("dat/cmrlouphiver.rds")
 
+dat[dat == 2] <- 1
+
+# Reset file
+cat("", file = "dat/mark.inp")
+
+# Output rows as strings
 for (row in 1:nrow(dat)) { 
   cat(paste(paste(dat[row,], collapse = ""), "1;\n"), 
       file = "dat/mark.inp", append = TRUE)
